4a160f40673ddf095248123494a103225f501593
RyoKitatani <ryo0929m@gmail.com>
2021-05-08 11:48:27 +0900

    Initial Commit

---
+++ SmileApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmileApp
+//
+//  Created by RyoKitatani on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmileApp/SmileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileAppApp.swift
+//  SmileApp
+//
+//  Created by RyoKitatani on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct SmileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
