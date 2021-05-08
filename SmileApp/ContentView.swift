1bdee102f12e238a0fd4becb7d6e60aa798545d3
RyoKitatani <ryo0929m@gmail.com>
2021-05-08 12:03:03 +0900

    SmileApp作成

--- SmileApp/ContentView.swift
+++ SmileApp/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "smiley")
+            .resizable()
+            .foregroundColor(.green)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .scaledToFit()
     }
 }
 
